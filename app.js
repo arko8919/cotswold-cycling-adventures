3c7efc4ce299438401f129fddaec7da53d4ca34d
Artur SD <arko8919@gmail.com>
2025-02-13 17:10:41 +0000

    changed api route from adventure to adventures

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.use(express.static(path.join(__dirname, 'src')));
 // Enable JSON parsing for incoming requests
 app.use(express.json({ limit: '10kb' }));
 
-app.use('/api/v1/adventure', adventureRouter);
+app.use('/api/v1/adventures', adventureRouter);
 
 module.exports = app;
